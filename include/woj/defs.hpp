6a987abe8429efa22a51e40e2d3af541e06df02d
Wojciech <69240079+cyber-wojtek@users.noreply.github.com>
2024-09-20 17:00:15 +0200

    stuff

--- include/woj/defs.hpp
+++ include/woj/defs.hpp
@@ -6,86 +6,88 @@
 
 // Define version detection for MSVC (_MSVC_LANG) and GCC/Clang (__cplusplus)
 #if defined(_MSC_VER) && defined(_MSVC_LANG)
-#define CPP_VERSION _MSVC_LANG
+	#define CPP_VERSION _MSVC_LANG
 #else
-#define CPP_VERSION __cplusplus
+	#define CPP_VERSION __cplusplus
 #endif
 
 // Check for C++ version and define corresponding macros
 #if CPP_VERSION >= 202602L
-#define HAS_CXX26 1
+	#define HAS_CXX26 1
 #endif
 
 #if CPP_VERSION >= 202302L
-#define HAS_CXX23 1
+	#define HAS_CXX23 1
 #endif
 
 #if CPP_VERSION >= 202002L
-#define HAS_CXX20 1
+	#define HAS_CXX20 1
 #endif
 
 #if CPP_VERSION >= 201703L
-#define HAS_CXX17 1
+	#define HAS_CXX17 1
 #endif
 
 #ifdef HAS_CXX17
-#define INLINE_VAR17 inline
+	#define INLINE_VAR17 inline
 #else
-#define INLINE_VAR17
+	#define INLINE_VAR17
 #endif
 
 // Define CONSTEXPR macros based on detected C++ version
 #if defined(HAS_CXX26)
-#define CONSTEXPR26 constexpr
+	#define CONSTEXPR26 constexpr
 #else
-#define CONSTEXPR26 inline
+	#define CONSTEXPR26 inline
 #endif
 
 #if defined(HAS_CXX23)
-#define CONSTEXPR23 constexpr
+	#define CONSTEXPR23 constexpr
 #else
-#define CONSTEXPR23 inline
+	#define CONSTEXPR23 inline
 #endif
 
 #if defined(HAS_CXX20)
-#define CONSTEXPR20 constexpr
-#define LIKELY [[likely]]
-#define UNLIKELY [[unlikely]]
+	#define CONSTEXPR20 constexpr
+	#define CONSTEVAL20 consteval
+	#define LIKELY [[likely]]
+	#define UNLIKELY [[unlikely]]
 #else
-#define CONSTEXPR20 inline
-#define LIKELY
-#define UNLIKELY
+	#define CONSTEXPR20 inline
+	#define CONSTEVAL20 inline
+	#define LIKELY
+	#define UNLIKELY
 #endif
 
 #if defined(HAS_CXX17)
-#define CONSTEXPR17    constexpr
-#define IF_CONSTEXPR17 if constexpr
-#define NODISCARD [[nodiscard]]
+	#define CONSTEXPR17    constexpr
+	#define IF_CONSTEXPR17 if constexpr
+	#define NODISCARD17 [[nodiscard]]
 #else
-#define CONSTEXPR17 inline
-#define IF_CONSTEXPR17 if (false)
-#define NODISCARD
+	#define CONSTEXPR17 inline
+	#define IF_CONSTEXPR17 if (false)
+	#define NODISCARD17
 #endif
 
 #if defined(HAS_CXX23)
-#define IF_CONSTEVAL if consteval
+	#define IF_CONSTEVAL if consteval
 #elif defined(HAS_CXX20)
-#define IF_CONSTEVAL if (std::is_constant_evaluated())
+	#define IF_CONSTEVAL if (std::is_constant_evaluated())
 #else
-#define IF_CONSTEVAL if (false)
+	#define IF_CONSTEVAL if (false)
 #endif
 
 #if defined(HAS_CXX23)
-#define ASSUME(...) [[assume(__VA_ARGS__)]]
+	#define ASSUME(...) [[assume(__VA_ARGS__)]]
 #elif defined(_MSC_VER) // Microsoft Visual C++
-#define ASSUME(...) __assume(__VA_ARGS__)
+	#define ASSUME(...) __assume(__VA_ARGS__)
 #elif defined(__clang__) // Clang
-#define ASSUME(...) __builtin_assume(__VA_ARGS__)
+	#define ASSUME(...) __builtin_assume(__VA_ARGS__)
 #elif defined(__GNUC__)
-#if __GNUC__ >= 13
-#define ASSUME(...)
-__attribute__((__assume__(__VA_ARGS__)))
-#else   
-#define ASSUME(...) do { if (!(__VA_ARGS__)) __builtin_unreachable(); } while (false);
+	#if __GNUC__ >= 13
+		#define ASSUME(...) __attribute__((__assume__(__VA_ARGS__)))
+	#else
+		#define ASSUME(...) do { if (!(__VA_ARGS__)) __builtin_unreachable(); } while (false);
+	#endif
 #endif
 #define ASSERT_ASSUME(...) assert(__VA_ARGS__); ASSUME(__VA_ARGS__);
--- include/woj/string.hpp
+++ include/woj/string.hpp
@@ -23,6 +23,66 @@
 
 namespace woj
 {
+	namespace impl
+	{
+		template <typename Elem, typename Traits = std::char_traits<Elem>>
+		class basic_streambuf_publicized : public std::basic_streambuf<Elem, Traits>
+		{
+		public:
+			using base_type = std::basic_streambuf<Elem, Traits>;
+
+			// Private
+			using base_type::base_type;
+			using base_type::operator=;
+			using base_type::swap;
+
+			// Public
+			using base_type::~basic_streambuf;
+			using base_type::pubseekoff;
+			using base_type::pubseekpos;
+			using base_type::pubsetbuf;
+			using base_type::pubimbue;
+			using base_type::getloc;
+			using base_type::in_avail;
+			using base_type::pubsync;
+			using base_type::sbumpc;
+			using base_type::sgetc;
+			using base_type::sgetn;
+			using base_type::snextc;
+			using base_type::sputbackc;
+#if !defined(HAS_CXX17)
+			using base_type::stossc;
+#endif // _HAS_OLD_IOSTREAMS_MEMBERS
+			using base_type::sungetc;
+			using base_type::sputc;
+			using base_type::sputn;
+
+			// Protected
+			using base_type::eback;
+			using base_type::gptr;
+			using base_type::pbase;
+			using base_type::pptr;
+			using base_type::egptr;
+			using base_type::gbump;
+			using base_type::setg;
+			using base_type::epptr;
+			using base_type::pbump;
+			using base_type::setp;
+			using base_type::overflow;
+			using base_type::pbackfail;
+			using base_type::showmanyc;
+			using base_type::underflow;
+			using base_type::uflow;
+			using base_type::xsgetn;
+			using base_type::xsputn;
+			using base_type::seekoff;
+			using base_type::seekpos;
+			using base_type::setbuf;
+			using base_type::sync;
+			using base_type::imbue;
+		};
+	}
+
 	#if defined(HAS_CXX20)
 	template <typename T>
 	concept char_type = std::is_same_v<T, char> || std::is_same_v<T, char8_t> || std::is_same_v<T, wchar_t> || std::is_same_v<T, char16_t> || std::is_same_v<T, char32_t>;
@@ -37,82 +97,12 @@ namespace woj
 	using is_char_v = typename is_char<T>::value;
 	#endif
 
-	template <typename T>
-	class reversed_iterators : public T
-	{
-		using iterator = typename T::iterator;
-		using const_iterator = typename T::const_iterator;
-		using reverse_iterator = typename T::reverse_iterator;
-		using const_reverse_iterator = typename T::const_reverse_iterator;
-
-		// ----- Iteration functions -----
-		CONSTEXPR20 iterator begin() noexcept
-		{
-			return this->rbegin();
-		}
-
-		CONSTEXPR20 const_iterator begin() const noexcept
-		{
-			return this->rbegin();
-		}
-
-		CONSTEXPR20 const_iterator cbegin() const noexcept
-		{
-			return this->crbegin();
-		}
-
-		CONSTEXPR20 iterator end() noexcept
-		{
-			return this->rend();
-		}
-
-		CONSTEXPR20 const_iterator end() const noexcept
-		{
-			return this->rend();
-		}
-
-		CONSTEXPR20 const_iterator cend() const noexcept
-		{
-			return this->crend();
-		}
-
-		CONSTEXPR20 iterator rbegin() noexcept
-		{
-			return this->begin();
-		}
-
-		CONSTEXPR20 const_iterator rbegin() const noexcept
-		{
-			return this->begin();
-		}
-
-		CONSTEXPR20 const_iterator crbegin() const noexcept
-		{
-			return this->cbegin();
-		}
-
-		CONSTEXPR20 iterator rend() noexcept
-		{
-			return this->end();
-		}
-
-		CONSTEXPR20 const_iterator rend() const noexcept
-		{
-			return this->end();
-		}
-
-		CONSTEXPR20 const_iterator crend() const noexcept
-		{
-			return this->cend();
-		}
-	};
-
 	namespace stack
 	{
 #if defined(HAS_CXX20)
-		template <char_type Elem, size_t Size>
+		template <char_type Elem, size_t Size, bool EnableIndexWrapAround = false>
 #else
-		template <typename Elem, size_t Size> requires is_char_v<Elem>
+		template <typename Elem, size_t Size, bool EnableIndexWrapAround = false> requires is_char_v<Elem>
 #endif
 		class string
 		{
@@ -135,7 +125,28 @@ namespace woj
 			template <typename IStrElem, typename IStrTraits = std::char_traits<IStrElem>>
 			CONSTEXPR20 friend std::ostream& operator<<(std::basic_ostream<IStrElem, IStrTraits>& ostr, const string& str)
 			{
-				ostr.write(str.data(), str.size());
+				using ostr_type = std::basic_ostream<IStrElem, IStrTraits>;
+				typename ostr_type::iostate state{ ostr_type::goodbit };
+
+				std::string s;
+				std::cout << s;
+
+				if (typename ostr_type::sentry{ ostr }) LIKELY
+				{
+					size_type pad = (ostr.width() <= 0) ? 0 : static_cast<size_type>(ostr.width());
+
+					if ((ostr.flags() & ostr_type::adjustfield) != ostr_type::left)
+					{
+						for ()
+					}
+				}
+				else
+					state |= ostr_type::badbit;
+
+				auto obj = static_cast<std::basic_streambuf<char>*>(nullptr);
+				obj->xsputn
+
+				ostr.rdbuf()->in_avail();
 				return ostr;
 			}
 
@@ -144,9 +155,8 @@ namespace woj
 			{
 				using istr_type = std::basic_istream<IStrElem, IStrTraits>;
 				using ctype = std::ctype<IStrElem>;
-
 				typename istr_type::iostate state{ istr_type::goodbit };
-
+				
 				if (typename istr_type::sentry{ istr }) LIKELY
 				{
 					const ctype& ctype_facet = std::use_facet<ctype>(istr.getloc());
@@ -154,9 +164,11 @@ namespace woj
 					typename IStrTraits::int_type chr = istr.rdbuf()->sgetc();
 					bool changed{ false };
 
-					for (size_type i = 0; i < (std::min)(str.size(), istr.width()); ++i, chr = istr.rdbuf()->snextc()) LIKELY
+					size_type i = 0;
+
+					for (; i < istr.width() > 0 ? (std::min)(str.max_size(), istr.width()) : str.max_size(); ++i, chr = istr.rdbuf()->snextc()) LIKELY
 					{
-						if (IStrTraits::eq_int_type(chr, IStrTraits::eof)) UNLIKELY
+						if (IStrTraits::eq_int_type(chr, IStrTraits::eof())) UNLIKELY
 						{
 							state |= istr_type::eofbit;
 							break;
@@ -172,6 +184,8 @@ namespace woj
 							changed = true;
 						}
 					}
+
+					str[i] = 0;
 
 					if (!changed) UNLIKELY
 						state |= istr_type::failbit;
@@ -243,7 +257,7 @@ namespace woj
 			 */
 			template <typename ElemPtr = const Elem* const>
 				requires std::is_pointer_v<ElemPtr>&&
-			std::is_same_v<std::remove_const_t<std::remove_pointer_t<std::remove_const_t<std::remove_pointer_t<ElemPtr>>>>, Elem> &&
+				std::is_same_v<std::remove_const_t<std::remove_pointer_t<std::remove_const_t<std::remove_pointer_t<ElemPtr>>>>, Elem> &&
 				(!std::is_array_v<ElemPtr>)
 				CONSTEXPR20 string(ElemPtr other) noexcept
 			{
@@ -258,7 +272,7 @@ namespace woj
 			 */
 			template <typename ElemPtr = const Elem* const>
 				requires std::is_pointer_v<ElemPtr>&&
-			std::is_same_v<std::remove_const_t<std::remove_pointer_t<std::remove_const_t<std::remove_pointer_t<ElemPtr>>>>, Elem> &&
+				std::is_same_v<std::remove_const_t<std::remove_pointer_t<std::remove_const_t<std::remove_pointer_t<ElemPtr>>>>, Elem> &&
 				(!std::is_array_v<ElemPtr>)
 				CONSTEXPR20 string(ElemPtr other, const size_type count) noexcept
 			{
@@ -349,73 +363,73 @@ namespace woj
 			 */
 			CONSTEXPR20 string& operator=(string&&) = delete;
 
-			CONSTEXPR20 Elem& operator[](const size_type index) noexcept
+			NODISCARD17 CONSTEXPR20 Elem& operator[](const size_type index) noexcept
 			{
 				return at(index);
 			}
 
-			CONSTEXPR20 const Elem& operator[](const size_type index) const noexcept
+			NODISCARD17 CONSTEXPR20 const Elem& operator[](const size_type index) const noexcept
 			{
 				return at(index);
 			}
 
 			// ----- Iteration functions -----
-			CONSTEXPR20 iterator begin() noexcept
+			NODISCARD17 CONSTEXPR20 iterator begin() noexcept
 			{
 				return buffer;
 			}
 
-			CONSTEXPR20 const_iterator begin() const noexcept
+			NODISCARD17 CONSTEXPR20 const_iterator begin() const noexcept
 			{
 				return buffer;
 			}
 
-			CONSTEXPR20 const_iterator cbegin() const noexcept
+			NODISCARD17 CONSTEXPR20 const_iterator cbegin() const noexcept
 			{
 				return begin();
 			}
 
-			CONSTEXPR20 iterator end() noexcept
+			NODISCARD17 CONSTEXPR20 iterator end() noexcept
 			{
 				return buffer + Size;
 			}
 
-			CONSTEXPR20 const_iterator end() const noexcept
+			NODISCARD17 CONSTEXPR20 const_iterator end() const noexcept
 			{
 				return buffer + Size;
 			}
 
-			CONSTEXPR20 const_iterator cend() const noexcept
+			NODISCARD17 CONSTEXPR20 const_iterator cend() const noexcept
 			{
 				return end();
 			}
 
-			CONSTEXPR20 reverse_iterator rbegin() noexcept
+			NODISCARD17 CONSTEXPR20 reverse_iterator rbegin() noexcept
 			{
 				return buffer + Size - 1;
 			}
 
-			CONSTEXPR20 const_reverse_iterator rbegin() const noexcept
+			NODISCARD17 CONSTEXPR20 const_reverse_iterator rbegin() const noexcept
 			{
 				return buffer + Size - 1;
 			}
 
-			CONSTEXPR20 const_reverse_iterator crbegin() const noexcept
+			NODISCARD17 CONSTEXPR20 const_reverse_iterator crbegin() const noexcept
 			{
 				return rbegin();
 			}
 
-			CONSTEXPR20 pointer rend() noexcept
+			NODISCARD17 CONSTEXPR20 pointer rend() noexcept
 			{
 				return buffer - 1;
 			}
 
-			CONSTEXPR20 const_reverse_iterator rend() const noexcept
+			NODISCARD17 CONSTEXPR20 const_reverse_iterator rend() const noexcept
 			{
 				return buffer - 1;
 			}
 
-			CONSTEXPR20 const_reverse_iterator crend() const noexcept
+			NODISCARD17 CONSTEXPR20 const_reverse_iterator crend() const noexcept
 			{
 				return rend();
 			}
@@ -554,7 +568,7 @@ namespace woj
 			 */
 			template <typename ElemPtr = const Elem* const>
 				requires std::is_pointer_v<ElemPtr>&&
-			std::is_same_v<std::remove_const_t<std::remove_pointer_t<std::remove_const_t<std::remove_pointer_t<ElemPtr>>>>, Elem> &&
+				std::is_same_v<std::remove_const_t<std::remove_pointer_t<std::remove_const_t<std::remove_pointer_t<ElemPtr>>>>, Elem> &&
 				(!std::is_array_v<ElemPtr>)
 				CONSTEXPR20 string& copy(ElemPtr other) noexcept
 			{
@@ -604,7 +618,7 @@ namespace woj
 			 */
 			template <typename ElemPtr = const Elem* const>
 				requires std::is_pointer_v<ElemPtr>&&
-			std::is_same_v<std::remove_const_t<std::remove_pointer_t<std::remove_const_t<std::remove_pointer_t<ElemPtr>>>>, Elem> &&
+				std::is_same_v<std::remove_const_t<std::remove_pointer_t<std::remove_const_t<std::remove_pointer_t<ElemPtr>>>>, Elem> &&
 				(!std::is_array_v<ElemPtr>)
 				CONSTEXPR20 string& copy(ElemPtr other, const size_type count) noexcept
 			{
@@ -728,29 +742,49 @@ namespace woj
 				return *this;
 			}
 
-			CONSTEXPR20 const string& as_const() const noexcept
+			NODISCARD17 CONSTEXPR20 const string& as_const() const noexcept
 			{
 				return *this;
 			}
 
-			[[nodiscard]] CONSTEXPR20 const decltype(buffer)& data() const noexcept
+			NODISCARD17 CONSTEXPR20 const decltype(buffer)& data() const noexcept
 			{
 				return buffer;
 			}
 
-			[[nodiscard]] CONSTEXPR20 decltype(buffer)& data() noexcept
+			NODISCARD17 CONSTEXPR20 decltype(buffer)& data() noexcept
 			{
 				return buffer;
 			}
 
-			[[nodiscard]] CONSTEXPR20 const decltype(buffer)& c_str() const noexcept
+			NODISCARD17 CONSTEXPR20 const decltype(buffer)& c_str() const noexcept
 			{
 				return buffer;
 			}
 
-			static consteval size_type size() noexcept
+			NODISCARD17 CONSTEXPR20 size_type str_size() const noexcept
+			{
+				size_type len{ mem_size() };
+
+				for (; len > 0 && buffer[len]; --len) [[likely]] {}
+
+				return len;
+
+			}
+
+			NODISCARD17 CONSTEXPR20 size_type size() const noexcept
+			{
+				return str_size();
+			}
+
+			NODISCARD17 static CONSTEVAL20 size_type mem_size() noexcept
 			{
 				return Size;
+			}
+
+			NODISCARD17 static CONSTEVAL20 size_type max_size() noexcept
+			{
+				return mem_size();
 			}
 		};
 	}
